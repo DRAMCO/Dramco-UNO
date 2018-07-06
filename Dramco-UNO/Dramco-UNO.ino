cd7b2ede76b05765f6d83b9c25c51fb4364eae5c
Bart Thoen <bart.thoen@kuleuven.be>
2018-07-06 09:38:28 +0200

    code cleanup

--- Dramco-UNO/Dramco-UNO.ino
+++ Dramco-UNO/Dramco-UNO.ino
@@ -16,6 +16,7 @@
 #define ACC_DATA_FORMAT 0x31
 #define ACC_START_BYTE  0x32  //X-Axis Data 0
 #define ACC_BYTES       0x06  //Number of databytes
+#define ACC_GAIN        0.00376390  //Convertion factor to g
 
 byte acc_buffer[ACC_BYTES];
 
@@ -29,14 +30,41 @@ DallasTemperature temp_sensors(&oneWire);   //Setup temperature sensor
 float temp_buffer[2]; 
 
 //LoRa module parameters
-// LoRaWAN NwkSKey, network session key
-static const PROGMEM u1_t NWKSKEY[16] 
-  = { 0x5C, 0xE3, 0x62, 0x82, 0x6B, 0xC4, 0xE2, 0x6B, 0xA9, 0xE8, 0x5C, 0xA8, 0x4C, 0xA3, 0x66, 0x68 };
-// LoRaWAN AppSKey, application session key
-static const u1_t PROGMEM APPSKEY[16]
-  = { 0xFC, 0xC0, 0x35, 0x91, 0x5B, 0x10, 0xE9, 0xB8, 0x81, 0xC9, 0x5A, 0x79, 0xA2, 0xAC, 0x43, 0xB8 };
-// LoRaWAN end-device address (DevAddr)
-static const u4_t DEVADDR = 0x26011638 ;
+#define DEVICE 1
+
+#define POWER_ENABLE_PIN 8
+#define MEASURE_INTERVAL 15000   //Measurement interval time in ms
+
+#if DEVICE == 1
+  // LoRaWAN NwkSKey, network session key
+  static const PROGMEM u1_t NWKSKEY[16] = { 0xEB, 0xD4, 0x3C, 0x3D, 0x5E, 0x32, 0xCD, 0xFF, 0x41, 0x65, 0xBB, 0x60, 0xCC, 0xD8, 0xD8, 0xE2 };
+  // LoRaWAN AppSKey, application session key
+  static const u1_t PROGMEM APPSKEY[16] = { 0xF2, 0xB0, 0xE4, 0xBF, 0x12, 0x8D, 0xAB, 0x8C, 0xD5, 0xB2, 0x58, 0x53, 0x3F, 0x9C, 0xB1, 0x2B };
+  // LoRaWAN end-device address (DevAddr)
+  static const u4_t DEVADDR = 0x26011343 ;
+#elif DEVICE == 2
+  // LoRaWAN NwkSKey, network session key
+  static const PROGMEM u1_t NWKSKEY[16] = { 0x1B, 0xC1, 0xDF, 0xBF, 0xF3, 0x8A, 0x71, 0x50, 0x5C, 0x2D, 0xE4, 0x9A, 0xF0, 0x30, 0xBD, 0x23 };
+  // LoRaWAN AppSKey, application session key
+  static const u1_t PROGMEM APPSKEY[16] = { 0xE5, 0x7F, 0xF1, 0x89, 0x8C, 0x95, 0xEC, 0x42, 0x72, 0x73, 0x0C, 0x54, 0x7A, 0xA0, 0x54, 0x40 };
+  // LoRaWAN end-device address (DevAddr)
+  static const u4_t DEVADDR = 0x260119E7 ;
+#elif DEVICE == 3
+  // LoRaWAN NwkSKey, network session key
+  static const PROGMEM u1_t NWKSKEY[16] = { 0x5C, 0xE3, 0x62, 0x82, 0x6B, 0xC4, 0xE2, 0x6B, 0xA9, 0xE8, 0x5C, 0xA8, 0x4C, 0xA3, 0x66, 0x68 };
+  // LoRaWAN AppSKey, application session key
+  static const u1_t PROGMEM APPSKEY[16] = { 0xFC, 0xC0, 0x35, 0x91, 0x5B, 0x10, 0xE9, 0xB8, 0x81, 0xC9, 0x5A, 0x79, 0xA2, 0xAC, 0x43, 0xB8 };
+  // LoRaWAN end-device address (DevAddr)
+  static const u4_t DEVADDR = 0x26011638 ;
+#elif DEVICE == 4
+  // LoRaWAN NwkSKey, network session key
+  static const PROGMEM u1_t NWKSKEY[16] = { 0xEA, 0x9C, 0x58, 0x94, 0xD6, 0x01, 0x68, 0xE2, 0x97, 0x87, 0x51, 0xFE, 0x6C, 0x64, 0x4B, 0xDB };
+  // LoRaWAN AppSKey, application session key
+  static const u1_t PROGMEM APPSKEY[16] = { 0x59, 0xE9, 0x64, 0x97, 0x1C, 0x4E, 0x23, 0x9C, 0x8B, 0xB9, 0xF5, 0xFF, 0xB6, 0x57, 0x74, 0xCE };
+  // LoRaWAN end-device address (DevAddr)
+  static const u4_t DEVADDR = 0x26011A6D ;
+#endif
+ 
 
 // These callbacks are only used in over-the-air activation, so they are
 // left empty here (we cannot leave them out completely unless
@@ -45,7 +73,9 @@ void os_getArtEui (u1_t* buf) { }
 void os_getDevEui (u1_t* buf) { }
 void os_getDevKey (u1_t* buf) { }
 
-static uint8_t mydata[] = "OK";
+#define LORA_TEMP 0x67
+#define LORA_ACC  0x71
+static uint8_t lora_stream[16];
 static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
@@ -60,8 +90,7 @@ const lmic_pinmap lmic_pins = {
     .dio = {2, 3, 4},
 };
 
-#define POWER_ENABLE_PIN 8
-#define MEASURE_INTERVAL 60000   //Measurement interval time in ms
+
 void setup()
 {
   //Enable Serial Port
@@ -80,14 +109,58 @@ void measure() {
   //Enable Accelero
   Wire.begin();        // join i2c bus (address optional for master)
   
-   //Put the ADXL345 into +/- 4G range by writing the value 0x01 to the DATA_FORMAT register.
-  writeTo(ACC_DATA_FORMAT, 0x01);
+   //Put the ADXL345 into +/- 2G range by writing the value 0x01 to the DATA_FORMAT register.
+  writeTo(ACC_DATA_FORMAT, 0x00);
    //Put the ADXL345 into Measurement Mode by writing 0x08 to the POWER_CTL register.
   writeTo(ACC_POWER_CTL, 0x08);
 
+  delay(200);
+
   //Start up temperature sensors
   temp_sensors.begin(); 
+  temp_sensors.setResolution(12); 
+  temp_sensors.requestTemperatures(); // Send the command to get temperature readings 
+  temp_buffer[0] = temp_sensors.getTempCByIndex(0);
+  temp_buffer[1] = temp_sensors.getTempCByIndex(1);
 
+  lora_stream[0] = 0;
+  lora_stream[1] = LORA_TEMP;
+  lora_stream[2] = (uint8_t)((int16_t)(temp_buffer[0]*10) >> 8);
+  lora_stream[3] = (uint8_t)((int16_t)(temp_buffer[0]*10) & 0xFF);
+
+  lora_stream[4] = 1;
+  lora_stream[5] = LORA_TEMP;
+  lora_stream[6] = (uint8_t)((int16_t)(temp_buffer[1]*10) >> 8);
+  lora_stream[7] = (uint8_t)((int16_t)(temp_buffer[1]*10) & 0xFF);
+  
+  Serial.print("Temp 1: ");
+  Serial.print(temp_buffer[0]);
+  Serial.print(" Temp 2: ");
+  Serial.println(temp_buffer[1]); 
+  
+  readFrom(ACC_START_BYTE, ACC_BYTES, acc_buffer); //read the acceleration data from the ADXL345
+    // each axis reading comes in 10 bit resolution, ie 2 bytes.  Least Significat Byte first!!
+  // thus we are converting both bytes in to one int
+  int acc_x = (int)(((((int)acc_buffer[1]) << 8) | acc_buffer[0]) * 3.76390);   
+  int acc_y = (int)(((((int)acc_buffer[3]) << 8) | acc_buffer[2]) * 3.76390);
+  int acc_z = (int)(((((int)acc_buffer[5]) << 8) | acc_buffer[4]) * 3.76390);
+
+  lora_stream[8] = 3;
+  lora_stream[9] = LORA_ACC;
+  lora_stream[10] = acc_x >> 8;
+  lora_stream[11] = acc_x & 0xFF;
+  lora_stream[12] = acc_y >> 8;
+  lora_stream[13] = acc_y & 0xFF;
+  lora_stream[14] = acc_z >> 8;
+  lora_stream[15] = acc_z & 0xFF;
+
+  Serial.print("x: ");
+  Serial.print(acc_x);
+  Serial.print(" y: ");
+  Serial.print(acc_y);
+  Serial.print(" z: ");
+  Serial.println(acc_z);
+  
   //Start Lora
 
   // LMIC init
@@ -156,17 +229,9 @@ void measure() {
 
   
   os_runloop_once();
-  
-  readAccel(); // read the x/y/z tilt
 
-  temp_sensors.requestTemperatures(); // Send the command to get temperature readings 
-  temp_buffer[0] = temp_sensors.getTempCByIndex(0);
-  temp_buffer[1] = temp_sensors.getTempCByIndex(1);
-  Serial.print("Temp 1: ");
-  Serial.print(temp_buffer[0]);
-  Serial.print(" Temp 2: ");
-  Serial.println(temp_buffer[1]); 
-  
+
+
   delay(5000); // only read every 0,5 seconds
   
   //Sleep
@@ -255,28 +320,12 @@ void do_send(osjob_t* j){
         Serial.println(F("OP_TXRXPEND, not sending"));
     } else {
         // Prepare upstream data transmission at the next possible time.
-        LMIC_setTxData2(1, mydata, sizeof(mydata)-1, 0);
+        LMIC_setTxData2(1, lora_stream, sizeof(lora_stream), 0);
         Serial.println(F("Packet queued"));
     }
     // Next TX is scheduled after TX_COMPLETE event.
 }
 //Accelero Functions
-void readAccel(){
-  readFrom(ACC_START_BYTE, ACC_BYTES, acc_buffer); //read the acceleration data from the ADXL345
-
-  // each axis reading comes in 10 bit resolution, ie 2 bytes.  Least Significat Byte first!!
-  // thus we are converting both bytes in to one int
-  int x = (((int)acc_buffer[1]) << 8) | acc_buffer[0];   
-  int y = (((int)acc_buffer[3]) << 8) | acc_buffer[2];
-  int z = (((int)acc_buffer[5]) << 8) | acc_buffer[4];
-  Serial.print("x: ");
-  Serial.print( x );
-  Serial.print(" y: ");
-  Serial.print( y );
-  Serial.print(" z: ");
-  Serial.println( z );
-}
-
 void writeTo(byte address, byte val){
   Wire.beginTransmission(ACC_ID); // start transmission to device 
   Wire.write(address);            // send register address
