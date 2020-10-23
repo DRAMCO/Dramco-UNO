e5208e1b67e11d3902b0b124ef08b502c450786b
Guus Leenders <leenders.guus@gmail.com>
2020-10-23 11:47:12 +0200

    Added acceleromter example

---
+++ libraries/Dramco-UNO/examples/SendOnMotion/SendOnMotion.ino
@@ -0,0 +1,16 @@
+#include <Dramco-UNO.h>
+
+LoraParam Device_EUI = "001449031C31FF8D";
+LoraParam Application_EUI = "70B3D57ED000EFD3";
+LoraParam App_Key = "1697F5AFE2242FFBA53E8383ED0ABB2D";
+
+void setup() {
+    DramcoUno.begin(Device_EUI, Application_EUI, App_Key);
+}
+
+void loop() {
+    DramcoUno.blink();
+    DramcoUno.addAcceleration();
+    DramcoUno.send();
+    DramcoUno.delayUntilMotion();
+}
